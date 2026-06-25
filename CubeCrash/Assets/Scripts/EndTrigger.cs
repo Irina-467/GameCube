23cb5fb7f7a9c8cab5fc1aa644b5ef74928ace88
Irina-467 <iarutyunyan21@gmail.com>
2026-06-26 01:22:56 +0400

    Added Animation

---
+++ CubeCrash/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,12 @@
+using UnityEditor.Media;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    void OnTriggerEnter()
+    {
+        gameManager.CompleteLevel1();
+    }
+}
